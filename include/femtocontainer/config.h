d27568f233c49c3be3ea5f418373636f1ce0f496
Koen Zandberg <koen@bergzand.net>
2021-12-10 16:51:22 +0100

    Initial import of the old bpf codebase

---
+++ include/femtocontainer/config.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2021 Inria
+ * Copyright (C) 2021 Koen Zandberg <koen@bergzand.net>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#ifndef FEMTOCONTAINER_CONFIG_H
+#define FEMTOCONTAINER_CONFIG_H
+
+#ifndef CONFIG_BPF_ENABLE_ALU32
+#define CONFIG_BPF_ENABLE_ALU32 (0)
+#endif
+
+#ifndef CONFIG_BPF_BRANCHES_ALLOWED
+#define CONFIG_BPF_BRANCHES_ALLOWED 200
+#endif
+
+
+
+#endif /* FEMTOCONTAINER_CONFIG_H */
+
